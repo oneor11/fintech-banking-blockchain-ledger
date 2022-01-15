cc431c68ebf389d5770ffaff7f01fbc4e42bb1f1
Jacob Rougeau <oneor11@hotmail.com>
2022-01-15 14:22:35 -0600

    Add UI logic to add record to block
    - Added UI inputs for sender, receiver, and amount
    - Constructed the record object from the user inputs
    - Constructed the new block and added it to the chain

--- app.py
+++ app.py
@@ -29,41 +29,36 @@ pychain = setup()
 # 4. Add an input area where you can get a value for `amount` from the user.
 # 5. As part of the Add Block button functionality, update `new_block` so that `Block` consists of an attribute named `record`, which is set equal to a `Record` that contains the `sender`, `receiver`, and `amount` values. The updated `Block`should also include the attributes for `creator_id` and `prev_hash`.
 
-# @TODO:
-# Delete the `input_data` variable from the Streamlit interface.
-input_data = st.text_input("Block Data")
 
-# @TODO:
-# Add an input area where you can get a value for `sender` from the user.
-# YOUR CODE HERE
+sender = st.text_input(
+    label = 'Sender: ',
+    value = '',
+    max_chars = 25
+)
 
-# @TODO:
-# Add an input area where you can get a value for `receiver` from the user.
-# YOUR CODE HERE
+receiver = st.text_input(
+    label = 'Receiver: ',
+    value = '',
+    max_chars = 25
+)
 
-# @TODO:
-# Add an input area where you can get a value for `amount` from the user.
-# YOUR CODE HERE
+amount = st.text_input(
+    label = 'Amount: ',
+    value = '0.00',
+)
 
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
     prev_block_hash = prev_block.hash_block()
 
-    # @TODO
-    # Update `new_block` so that `Block` consists of an attribute named `record`
-    # which is set equal to a `Record` that contains the `sender`, `receiver`,
-    # and `amount` values
     new_block = Block(
-        data=input_data,
-        creator_id=42,
-        prev_hash=prev_block_hash
-    )
+        record = Record(sender=sender, receiver=receiver, amount=amount),
+        creator_id = 42,
+        prev_hash = prev_block_hash)
 
     pychain.add_block(new_block)
     st.balloons()
 
-################################################################################
-# Streamlit Code (continues)
 
 st.markdown("## The PyChain Ledger")
 
