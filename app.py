996784eb082d7139d92a9e5388d60d01b30b7d0c
Jacob Rougeau <oneor11@hotmail.com>
2022-01-15 15:33:15 -0600

    Add docstring and code formatting to app.py
    Added docstrings and rearranged the code in app.py to be more logical.

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+""" Contains UI presentation and input logic.
+
+    Leverages Streamlit to define input controls that leverage
+    blockchain classes that control the addition of Blocks to the
+    PyChain blockchain.
+
+"""
 import streamlit as st
 import pandas as pd
 from blockchain.pychain import PyChain
@@ -10,25 +17,12 @@ def setup():
     return PyChain([Block("Genesis", 0)])
 
 
-st.markdown("# PyChain")
+st.markdown("# Decentralized Banking Powered by PyChain")
 st.markdown("## Store a Transaction Record in the PyChain")
 
 pychain = setup()
 
-################################################################################
-# Step 3:
-# Add Relevant User Inputs to the Streamlit Interface
-
-# Code additional input areas for the user interface of your Streamlit
-# application. Create these input areas to capture the sender, receiver, and
-# amount for each transaction that you’ll store in the `Block` record.
-# To do so, complete the following steps:
-# 1. Delete the `input_data` variable from the Streamlit interface.
-# 2. Add an input area where you can get a value for `sender` from the user.
-# 3. Add an input area where you can get a value for `receiver` from the user.
-# 4. Add an input area where you can get a value for `amount` from the user.
-# 5. As part of the Add Block button functionality, update `new_block` so that `Block` consists of an attribute named `record`, which is set equal to a `Record` that contains the `sender`, `receiver`, and `amount` values. The updated `Block`should also include the attributes for `creator_id` and `prev_hash`.
-
+# INPUT SECTION ===========================================
 
 sender = st.text_input(
     label = 'Sender: ',
@@ -59,11 +53,21 @@ if st.button("Add Block"):
     pychain.add_block(new_block)
     st.balloons()
 
-
 st.markdown("## The PyChain Ledger")
+# =========================================================
+
+
+# INFORMATIONAL SECTION ===================================
 
 pychain_df = pd.DataFrame(pychain.chain).astype(str)
 st.write(pychain_df)
+
+if st.button("Validate Chain"):
+    st.write(pychain.is_valid())
+#==========================================================
+
+
+# SIDEBAR SECTION =========================================
 
 difficulty = st.sidebar.slider("Block Difficulty", 1, 5, 2)
 pychain.difficulty = difficulty
@@ -74,34 +78,4 @@ selected_block = st.sidebar.selectbox(
 )
 
 st.sidebar.write(selected_block)
-
-if st.button("Validate Chain"):
-    st.write(pychain.is_valid())
-
-################################################################################
-# Step 4:
-# Test the PyChain Ledger by Storing Records
-
-# Test your complete `PyChain` ledger and user interface by running your
-# Streamlit application and storing some mined blocks in your `PyChain` ledger.
-# Then test the blockchain validation process by using your `PyChain` ledger.
-# To do so, complete the following steps:
-
-# 1. In the terminal, navigate to the project folder where you've coded the
-#  Challenge.
-
-# 2. In the terminal, run the Streamlit application by
-# using `streamlit run pychain.py`.
-
-# 3. Enter values for the sender, receiver, and amount, and then click the "Add
-# Block" button. Do this several times to store several blocks in the ledger.
-
-# 4. Verify the block contents and hashes in the Streamlit drop-down menu.
-# Take a screenshot of the Streamlit application page, which should detail a
-# blockchain that consists of multiple blocks. Include the screenshot in the
-# `README.md` file for your Challenge repository.
-
-# 5. Test the blockchain validation process by using the web interface.
-# Take a screenshot of the Streamlit application page, which should indicate
-# the validity of the blockchain. Include the screenshot in the `README.md`
-# file for your Challenge repository.
+#==========================================================
