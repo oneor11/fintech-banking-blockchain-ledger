a92c480e900bacded7466d02440e90250c857ff8
Jacob Rougeau <oneor11@hotmail.com>
2022-01-15 15:25:39 -0600

    Add docstring to record.py
    Added docstrings to the Record class and its attributes.

--- blockchain/record.py
+++ blockchain/record.py
@@ -1,7 +1,40 @@
+"""Defines the Record object.
+
+    The Record object defines the minimum banking transaction which is
+    the sender, receiver, and amount.  It is one of the attributes in
+    the Block object.
+
+    Typical usage example:
+    record =  Record('Bank of America', 'Capital One', 5003.23)
+
+"""
+
 from dataclasses import dataclass
 
 @dataclass
 class Record:
+    """Defines the Record object.
+
+    The Record object defines the minimum banking transaction which is
+    the sender, receiver, and amount.  It is one of the attributes in
+    the Block object.
+
+    Typical usage example:
+    record =  Record('Bank of America', 'Capital One', 5003.23)
+    
+    Attributes:
+        sender: str
+            Sender, or originator, of the banking transaction.
+        
+        receiver: str
+            Receiver, or recipient, of the banking transaction.
+        
+        amount: float
+    
+    Returns:
+        None
+    """
+
     sender: str
     receiver: str
     amount: float
